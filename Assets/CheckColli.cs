e10c6b9a9fb8823ce614f6d09ae51b03a8dcad09
Zackary P. T. Sin <zackarysin@gmail.com>
2020-02-01 15:01:00 +0800

    Vuforia Integration

---
+++ Assets/CheckColli.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CheckColli : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.name == "Rock2")
+        {
+            gameObject.transform.GetChild(0).GetComponent<Renderer>().material.SetColor("_Color", Color.red);
+        }
+
+        if (other.gameObject.name == "Rock4A")
+        {
+            gameObject.transform.GetChild(0).GetComponent<Renderer>().material.SetColor("_Color", Color.yellow);
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        gameObject.transform.GetChild(0).GetComponent<Renderer>().material.SetColor("_Color", Color.white);
+    }
+
+}
